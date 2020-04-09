ef7d716fe23caf6ffbe82c5ef33772ed161d9d30
Maksym Teplyi <teplymax@gmail.com>
2020-04-09 11:26:11 +0300

    added homework#12 and homework#13

---
+++ Lesson12_JS/Homework/js/main.js
@@ -0,0 +1,60 @@
+console.log(""+1+0);
+console.log(""-1+0);
+console.log(true+false);
+console.log(6/"3");
+console.log("2"*"3");
+console.log(4+5+"px");
+console.log("$"+4+5);
+console.log("4"-2);
+console.log("4px"-2);
+console.log(7/0);
+console.log(" -9/n"+5);
+console.log(" -9/n"-5);
+console.log(5 && 2);
+console.log(2 && 5);
+console.log(5 || 0);
+console.log(0 || 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(null == "\n0\n");
+console.log(+null == +"\n0\n");
+//null undefined string symbol boolean number
+// console.log(typeof 0);
+// console.log(typeof '');
+// console.log(typeof {});
+// console.log(typeof function(){});
+// console.log(typeof []);
+// console.log(typeof null);
+// console.log(typeof undefined);
+//Приведення типів
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean({}));
+// console.log(Boolean([]));
+// console.log(Boolean(''));
+// console.log(Boolean('fddf'));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+//Оператори порівняння
+// console.log(false==0);
+// console.log(false===0);
+// console.log(0=='0');
+// console.log(false==[]);
+// console.log(false=={});
+// console.log(''==0);
+// console.log(''==[]);
+// console.log(''=={});
+// console.log(0==[]);
+// console.log(0=={});
+// console.log(null==0);
+// console.log(undefined==0);
+//строки й числа
+// console.log(2+3);
+// console.log(14+2+4+'5');
+// console.log('2'+2+2)
+// console.log(2+2+'2')
+// console.log(1+3+'px');
+// console.log('px'+3+32);
+// console.log('14px'+2);
+// console.log(''+1+0);
+// console.log(''-1-3);
---
+++ Lesson13_JS_Operators/js/script.js
@@ -0,0 +1,41 @@
+// let a=prompt("vvedit chyslo");
+// if(a<30 && a>20) {console.log('true')} 
+// else{console.log('false')};
+// console.log(a);
+// (a==null)?console.log('nichogo ne vvely') 
+//  : console.log(a)
+// let b = prompt("vvedit chyslo");
+// console.log(b);
+// switch (b) {
+// case 1:
+// console.log ('a');
+// break;
+// case 2:
+// console.log ('b');
+// break;
+// case 3:
+// console.log ('c');
+// break;
+// default:
+// console.log ('z');
+// break;
+// }
+// for(let i=1; i<=9;i++)
+// {
+// console.log(i**2);
+// }
+// let i=1;
+// while(i<=9)
+// {
+//     console.log(i**2);
+//     i++;
+// }
+// function Checkwaight(weight){
+//     (weight>100)? console.log('weight>100kg'): console.log('weight<100kg')
+// }
+// let weight=prompt("vvedit vagu");
+// Checkwight(weight);
+let a= function(b,c){if (b>c) {return b;} else{return c}}
+let b=prompt("chyslo 1");
+let c=prompt("chyslo 2");
+console.log(a(b,c));
